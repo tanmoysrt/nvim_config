4fbcf09dd725e3ea1c381748320f65c46d359822
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2024-10-18 01:53:04 +0530

    feat: neotree toggle support added

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -3,6 +3,20 @@ return {
     branch = "v3.x",
     dependencies = { "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons", "MunifTanjim/nui.nvim" },
     config = function()
-        vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>')
+        local neotree_revealed = false
+
+        -- Function to toggle Neotree
+        local function toggle_neotree()
+            if neotree_revealed then
+                vim.cmd('Neotree close')
+                neotree_revealed = false
+            else
+                vim.cmd('Neotree filesystem reveal left')
+                neotree_revealed = true
+            end
+        end
+
+        -- Set keymap to toggle Neotree
+        vim.keymap.set('n', '<C-n>', toggle_neotree)
     end
 }
