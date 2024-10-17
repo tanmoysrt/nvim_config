9d6f8bf388c7f757ec32022027ef88a9ecb6c739
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2024-10-18 00:46:20 +0530

    feat: init config

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,17 @@
+return { {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+        local config = require("nvim-treesitter.configs")
+        config.setup({
+            ensure_installed = { "lua", "vim", "python", "javascript", "html", "go" },
+            sync_install = false,
+            highlight = {
+                enable = true
+            },
+            indent = {
+                enable = true
+            }
+        })
+    end
+} }
